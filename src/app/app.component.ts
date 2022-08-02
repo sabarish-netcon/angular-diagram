10d3f16402b08e71c21bd28f68a0b645c576630b
sabarish <sabharshin@gmail.com>
2022-08-02 12:48:48 +0530

    feat: initial diagram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import {
+  FlowShapeModel,
+  ImageElement,
+  NodeModel,
+  OrthogonalSegmentModel,
+} from '@syncfusion/ej2-angular-diagrams';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-diagram';
+  public segment1: OrthogonalSegmentModel = [
+    { direction: 'Right', length: 30 },
+    { direction: 'Bottom', length: 300 },
+  ];
+  public segment2: OrthogonalSegmentModel = [
+    { length: 30, direction: 'Left' },
+    { length: 200, direction: 'Top' },
+  ];
+  public terminatorShape: FlowShapeModel = {
+    type: 'Flow',
+    shape: 'Terminator',
+  };
+  public processShape: FlowShapeModel = {
+    type: 'Flow',
+    shape: 'Process',
+  };
+  public decisionShape: FlowShapeModel = {
+    type: 'Flow',
+    shape: 'Decision',
+  };
+  public preDefinedShape: FlowShapeModel = {
+    type: 'Flow',
+    shape: 'PreDefinedProcess',
+  };
+  public imageShape = {
+    type: 'Image',
+    source:
+      'https://www.syncfusion.com/content/images/nuget/sync_logo_icon.png',
+  };
+  public defaultNodeValues(defaultnode: NodeModel): NodeModel {
+    defaultnode.height = 50;
+    defaultnode.width = 200;
+    defaultnode.offsetX = 150;
+    return defaultnode;
+  }
+  OnSelectionChange(params) {
+    console.log('OnSelectionChange: ', params);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DiagramModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
